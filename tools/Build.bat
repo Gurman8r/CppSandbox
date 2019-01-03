25825f20b10c918875910256f5d6bffd1dff2b7e
Melody <gurman8r@gmail.com>
2019-01-02 20:31:45 -0500

    im fickle

--- tools/Build.bat
+++ tools/Build.bat
@@ -5,8 +5,7 @@ cls
 rem Initialize
 :init
 set WorkingDir=%cd%\
-set VSVersion=Enterprise\
-set MSBuildPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\%VSVersion%
+set MSBuildPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\
 set MSBuildExe=msbuild.exe
 set VCTargetsPath=%MSBuildPath%Common7\IDE\VC\VCTargets\
 set VCVarsPath=%MSBuildPath%VC\Auxiliary\Build\
