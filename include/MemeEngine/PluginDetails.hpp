7688c635f458a770ed42737431fd26eafb44a1af
Gurman8r <Gurman8r@gmail.com>
2019-04-15 13:49:02 -0400

    placeholders

--- include/MemeEngine/PluginDetails.hpp
+++ include/MemeEngine/PluginDetails.hpp
@@ -1,5 +1,20 @@
 #ifndef _ML_PLUGIN_DETAILS_HPP_
 #define _ML_PLUGIN_DETAILS_HPP_
 
+#include <MemeEngine/PluginAPI.hpp>
+#include <MemeCore/ITrackable.hpp>
+
+namespace ml
+{
+	struct ML_ENGINE_API PluginDetails final
+		: public ITrackable
+	{
+		int32_t apiVersion;
+		CString fileName;
+		CString className;
+		CString pluginName;
+		CString pluginVersion;
+	};
+}
 
 #endif // !_ML_PLUGIN_DETAILS_HPP_
