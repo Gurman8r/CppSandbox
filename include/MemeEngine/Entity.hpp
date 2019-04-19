38339b214839cb7986eed134c0467c2efe11c64d
Melody <gurman8r@gmail.com>
2019-04-18 22:13:34 -0400

    fixed Entity.hpp

--- include/MemeEngine/Entity.hpp
+++ include/MemeEngine/Entity.hpp
@@ -1,5 +1,5 @@
-#ifndef _ML_I_ENTITY_HPP_
-#define _ML_I_ENTITY_HPP_
+#ifndef _ML_ENTITY_HPP_
+#define _ML_ENTITY_HPP_
 
 #include <MemeEngine/Export.hpp>
 #include <MemeCore/ITrackable.hpp>
@@ -146,4 +146,4 @@ namespace ml
 	/* * * * * * * * * * * * * * * * * * * * */
 }
 
-#endif // !_ML_I_ENTITY_HPP_
+#endif // !_ML_ENTITY_HPP_
